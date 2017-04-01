29af2235d1c2170c1583cb9fbf44fd2dc155a626
Meghan King <mfking@uvm.edu>
2017-04-01 15:32:38 -0400

    Index upload photos

--- index.php
+++ index.php
@@ -6,7 +6,34 @@ include "nav.php";
         <h2>About</h2>
         <p>New to campus? Lost? Looking for food? Take a photo of the closest building and find out where you are and some cool campus info!</p>
         <!-- Upload or take photo button -->
-        <button type="button">Upload Image</button>
+    <script>
+   function previewFile(){
+       var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+            } else {
+           preview.src = "";
+        }
+    }
+
+  previewFile();  //calls the function named previewFile()
+  </script>
+        <form action="upload.php" method="post" enctype="multipart/form-data" onchange="previewFile()">
+        Select image to upload: 
+        <input type="file" name="fileToUpload"
+            id="fileToUpload"> <br>
+            <img src="images/upload11.png" height="100" alt="Upload an Image"><br>
+            <input type="submit" value="Upload"
+            name="submit">
+    </form>
+
         <button type="button" onclick="alert('Allow access to camera')">Take Photo</button>
         <p></p>
         <!-- Campus Map -->
